dede6b74f1c18d63e589ae70f9551c788de70e61
phung loc <phungloc6102003@gmail.com>
2025-01-18 00:15:01 +0700

    use create theme MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import TabIndicator from '@mui/material/Tabs';
+import Typography from '@mui/material/Typography'
 function App() {
   return (
     <>
+    <Typography variant='caption' color={'text.primary'}>Hello World</Typography>
       <div>LocDev</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Container</Button>
