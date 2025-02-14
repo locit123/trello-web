14a3fd3287b4e6830ca62cbcdcb46e75990b78cb
phung loc <phungloc6102003@gmail.com>
2025-02-14 14:04:45 +0700

    dark_light_p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import TabIndicator from '@mui/material/Tabs';
+import Button from '@mui/material/Button'
+import DeleteIcon from '@mui/icons-material/Delete'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button onClick={() => {
+      setMode(mode === 'light' ? 'dark' : 'light')
+    }}>
+      {mode === 'light' ? 'dark': 'light'}
+    </Button>
+  )
+}
+
 function App() {
-  let x = 'loc';
   return (
     <>
-    <Typography variant='caption' color={'text.primary'}>Hello World</Typography>
+      <ModeToggle />
+      <hr/>
+      <Typography variant='caption' color={'text.primary'}>Hello World</Typography>
       <div>LocDev</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Container</Button>
       <Button variant="outlined">Outlined</Button>
       <DeleteIcon />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
