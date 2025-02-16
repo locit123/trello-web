72f9254d22b6d6683bd3a14e5f538a176b1a83b6
phung loc <phungloc6102003@gmail.com>
2025-02-16 22:17:44 +0700

    app bar p2

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,44 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 // Create a theme instance.
 const theme = extendTheme({
+  components:{
+    MuiButton:{
+      styleOverrides:{
+        root:{
+          textTransform:'none'
+        }
+      }
+    },
+    MuiInputLabel:{
+      styleOverrides:{
+        root:({ theme }) => ({
+          color:theme.palette.primary.main,
+          fontSize:'0.875rem'
+        })
+      }
+    },
+    MuiOutlinedInput:{
+      styleOverrides:{
+        root:({ theme }) => {
+          return {
+            color:theme.palette.primary.main,
+            fontSize:'0.875rem',
+            '.MuiOutlinedInput-notchedOutline':{
+              borderColor:theme.palette.primary.light
+            },
+            '&:hover':{
+              '.MuiOutlinedInput-notchedOutline':{
+                borderColor:theme.palette.primary.main
+              }
+            },
+            '& fieldset':{
+              borderWidth:'1px !important'
+            }
+          }
+        }
+      }
+    }
+  },
   trello:{
     appBarHeight:'58px',
     boardBarHeight:'60px'
